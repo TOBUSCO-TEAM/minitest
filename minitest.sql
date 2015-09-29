64682eb65ef940392f30ec7ef4814f0a3d3c3ef2
kaze94phoenix <phoenix.kazekage@gmail.com>
2015-09-29 11:54:49 +0200

    Merge origin/master
    Conflicts:
    	integracao de template
    	base de dados

--- minitest.sql
+++ minitest.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 22-Set-2015 às 08:59
+-- Generation Time: 29-Set-2015 às 11:50
 -- Versão do servidor: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -41,7 +41,7 @@ CREATE TABLE IF NOT EXISTS `members` (
 --
 
 INSERT INTO `members` (`id`, `name`, `function`, `username`, `password`, `task_count`) VALUES
-(1, 'Celso', 'Programador', 'kaze94phoenix', '27b4cb63', 3),
+(1, 'Celso', 'Programador', 'kaze94phoenix', '27b4cb63', 5),
 (2, 'Aminissa', 'Administadora de Base de Dados', 'amysupeta', 'dmi2015', 2),
 (3, 'Frenque', 'Web Designer', 'flowzzy', 'dmi2015', 2),
 (4, 'Sheila', 'Programador', 'sheyla', 'dmi2015', 1),
@@ -105,7 +105,7 @@ CREATE TABLE IF NOT EXISTS `tasks` (
   `project_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `member_id` (`member_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
 
 --
 -- Extraindo dados da tabela `tasks`
@@ -119,7 +119,12 @@ INSERT INTO `tasks` (`id`, `title`, `description`, `deadline`, `member_id`, `pro
 (5, 'sadasdasda', 'asdadsa', '2015-09-08', 2, 0),
 (6, 'wewadasdasd', 'asdsadas', '2015-09-08', 3, 0),
 (7, 'ldasdaasda', 'asasdasd', '2015-09-10', 4, 2),
-(8, 'adadw', 'asdawdawd', '2015-09-11', 1, 0);
+(8, 'adadw', 'asdawdawd', '2015-09-11', 1, 0),
+(9, 'hh', 'h', '2015-09-22', 1, 1),
+(10, 'Amy', 'eu sei', '2015-09-22', 1, 1),
+(11, 'sdsdsdsd', 'dd', '2015-01-01', NULL, 0),
+(12, 'sdsdsdsd', 'dd', '2015-01-01', NULL, 0),
+(13, 'sdsdsdsd', 'dd', '2015-01-01', NULL, 0);
 
 -- --------------------------------------------------------
 
@@ -137,7 +142,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`id`),
   KEY `member_id` (`member_id`),
   KEY `role_id` (`role_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=38 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=39 ;
 
 --
 -- Extraindo dados da tabela `users`
@@ -147,7 +152,8 @@ INSERT INTO `users` (`id`, `username`, `password`, `member_id`, `role_id`, `imag
 (32, 'kaze', '588ba4b683a4ea1edf1c6b755c040c60c3cb6e74', 1, 1, 0x3339353133315f31303135313933393330383235323133375f3835303938323235325f6e2e6a7067),
 (33, 'asd', '0a5aed2314dfe5f7d66e42454471eefe8743db6f', 2, 0, ''),
 (35, 'sas', '0a5aed2314dfe5f7d66e42454471eefe8743db6f', 2, 0, ''),
-(37, 'sheyla', 'd40b3d0f5895ff43563b4b29912c6ab8efd8fed6', 4, 2, 0x31313730373631355f3933383036353938393630303432355f383430303131333537333639353439333837345f6e2e6a7067);
+(37, 'sheyla', 'd40b3d0f5895ff43563b4b29912c6ab8efd8fed6', 4, 2, 0x31313730373631355f3933383036353938393630303432355f383430303131333537333639353439333837345f6e2e6a7067),
+(38, 'abacome', '588ba4b683a4ea1edf1c6b755c040c60c3cb6e74', 6, 1, 0x31323033363734395f3732323031363633343539353437325f353336323432323830353238333230313632305f6e2e6a7067);
 
 --
 -- Constraints for dumped tables
